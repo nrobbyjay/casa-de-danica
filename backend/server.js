d6605f43a24d1caa0fd42a8a69e7f2f5e674c354
Robby Jay Navarro <nrobbyjay@outlook.com>
2025-06-05 19:05:31 +0800

    Created backend server's test routes: payments, user and booking

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const booking = require('./routes/booking')
+app.use('/api/booking', booking)
+
+const payment = require('./routes/payment')
+app.use('/api/payment', payment)
+
+const user = require('./routes/user')
+app.use('/api/user', user)
+
+
+
+const server = app.listen(process.env.PORT, ()=>{
+    console.log(`Service is starting at port ${process.env.PORT}`)
+})
+server.on('error', (e)=>{
+    console.log(`Problem starting server: ${e}`)
+})
